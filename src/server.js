3bdd6d39bbcf9c4b63a1f51a559a2eaf5a2fe4fa
momo123456Ss <nino02022002@gmail.com>
2023-06-28 19:16:05 +0700

    EJS config

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
-const express = require('express')
-
+// const express = require('express')
+// const path = require('path')
+import express from 'express';
+import configViewEngine from './configs/viewEngine';
 const app = express()
 const port = 8080
 
+configViewEngine(app);
+
 app.get('/', (req, res) => {
-    res.send('Hello World!123')
+    res.render('index.ejs')
 })
 app.get('/about', (req, res) => {
     res.send('ThienHuynh')
